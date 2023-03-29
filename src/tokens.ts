2fac1b4e25bd4a6387df50e494f5da5918be96d4
Diego Henrique <diegoheflavia@gmail.com>
2023-03-29 15:05:19 -0300

    Add new tokens

--- src/tokens.ts
+++ src/tokens.ts
@@ -11,5 +11,11 @@ export type MaskTokens = Record<string, MaskToken>
 export const tokens: MaskTokens = {
   '#': { pattern: /[0-9]/ },
   '@': { pattern: /[a-zA-Z]/ },
-  '*': { pattern: /[a-zA-Z0-9]/ }
+  '*': { pattern: /[a-zA-Z0-9]/ },
+  '0': { pattern: /[0-9]/, optional: true },
+  '9': { pattern: /[0-9]/, repeated: true },
+  'A': { pattern: /[a-zA-Z]/, transform: (v) => v.toUpperCase() },
+  'a': { pattern: /[a-zA-Z]/, transform: (v) => v.toLowerCase() },
+  'S': { pattern: /[a-zA-Z0-9]/, transform: (v) => v.toUpperCase() },
+  's': { pattern: /[a-zA-Z0-9]/, transform: (v) => v.toLowerCase() },
 }
